29e4eacebbaf0f59685b423432fe8e1079f93eee
Adrien Raimbault <adrien.raimbault@etu.umontpellier.fr>
2023-11-12 20:26:08 +0100

    feat: add screen processing to project (WIP)

--- main.py
+++ main.py
@@ -1,27 +1,64 @@
 import pafy
 import cv2
+import numpy as np
 
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+def askUserForVideo():
+    print('Enter video url:')
+    return input() or "https://www.youtube.com/watch?v=cmkAbDUEoyA"
 
 def main():
 
-    # # 1°) Import live from youtube or webcam
-    url = "https://www.youtube.com/watch?v=3kPH7kTphnE"
+    # 1°) Import live from youtube or webcam
+    # example_url = "https://www.youtube.com/watch?v=3kPH7kTphnE"
+    url = askUserForVideo()
     video = pafy.new(url)
     print(video.title)
     best = video.getbest(preftype="mp4")
-    
-    # 2°) Show video
+
+    # 2°) Process video
+
+    # 3°) Show video
     capture = cv2.VideoCapture(best.url)
     while True:
         grabbed, frame = capture.read()
+
         key = cv2.waitKey(1) & 0xFF
         if frame is None or key == ord("q"):
-                # if True break the infinite loop
-                break
-        # do something with frame here
-        image = cv2.rectangle(frame, (100, 100), (200, 200), (0, 255, 0), 2)
-        cv2.imshow("Output Frame", image)
+            # if True break the infinite loop
+            break
 
-    # 3°) Process video
+        # detect people in the image
+        # returns the bounding boxes for the detected objects
+        boxes, weights = hog.detectMultiScale(frame, winStride=(8, 8))
+
+        boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+
+        for (xA, yA, xB, yB) in boxes:
+            # display the detected boxes in the colour picture
+            cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+            
+         # font 
+        font = cv2.FONT_HERSHEY_SIMPLEX 
+        
+        # org 
+        org = (50, 50) 
+        
+        # fontScale 
+        fontScale = 1
+        
+        # Blue color in BGR 
+        color = (255, 0, 0) 
+        
+        # Line thickness of 2 px 
+        thickness = 2
+        
+        print(len(boxes))
+        # do something with frame here
+        cv2.imshow("Output Frame", frame)
+
 
 main()
